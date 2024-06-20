cd2bd8c227f62ce9f511d7a3fd74591406f42ae5
DieGoArt30 <diehgohhz.30@gmail.com>
2024-06-19 22:07:18 -0400

    Repository initialization

---
+++ todoproject/tasks/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
+from .models import Task
+from .forms import TaskForm
+
+def inicio_sesion(request):
+    if request.method == 'GET':
+        return render(request,'login.html')
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    pending_tasks = tasks.filter(status='P').count()  # Contar solo las tareas pendientes
+    return render(request, 'task/task_list.html', {'tasks': tasks, 'pending_tasks': pending_tasks})
+
+@login_required
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    return render(request, 'task/task_detail.html', {'task': task})
+
+@login_required
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm()
+    return render(request, 'task/task_form.html', {'form': form})
+
+@login_required
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'task/task_form.html', {'form': form})
+
+@login_required
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    task.delete()
+    return redirect('task_list')
+
+@login_required
+def task_toggle_complete(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    if task.status == 'P':
+        task.status = 'C'
+    else:
+        task.status = 'P'
+    task.save()
+    return redirect('task_list')
+
+def landing_page(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'task/landing_page.html', {'form': form})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'task/register.html', {'form': form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('landing_page')
